8e858210a4718b6ed84ddd57684488fea74b4819
UNNuwantha <www.nadun50@gmail.com>
2026-04-15 14:08:39 +0530

    Developing the Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import Navbar from './layout/Navbar'
-import Hero from './sections/Hero'
-import About from './sections/About'
-import Experience from './sections/Experience'
-import Projects from './sections/Projects'
-import Testimonials from './sections/Testimonials'
-import Contact from './sections/Contact'
+import { Navbar } from "@/layout/Navbar";
+import { Hero } from "@/sections/Hero";
+import { About } from "@/sections/About";
+import { Projects } from "@/sections/Projects";
+import { Experience } from "@/sections/Experience";
+import { Testimonials } from "@/sections/Testimonials";
+import { Contact } from "@/sections/Contact";
+import { Footer } from "./layout/Footer";
 
-
-
-const App = () => {
+function App() {
   return (
-    <div className='min-h-screen overflow-x-hidden'>
+    <div className="min-h-screen overflow-x-hidden">
       <Navbar />
       <main>
         <Hero />
@@ -21,8 +19,9 @@ const App = () => {
         <Testimonials />
         <Contact />
       </main>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
